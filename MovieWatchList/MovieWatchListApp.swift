80e13baef508bc102c5e7513a99b0a197ce8e3cd
Akhil Madiri <akhilmadiri1@gmail.com>
2024-08-29 08:39:10 -0400

    Initial Commit

---
+++ MovieWatchList/MovieWatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieWatchListApp.swift
+//  MovieWatchList
+//
+//  Created by Akhil Madiri on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieWatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
